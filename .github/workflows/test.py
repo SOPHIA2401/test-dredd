b9897cac8f84af7e2c52da9dec7ea42c073bd57d
Sophia Garg <sofigarg@amazon.com>
2022-05-12 20:17:39 +0530

    65
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -3,6 +3,7 @@ import os
 for i in range(0,6):
     command = "curl 'https://admin:admin@localhost:9200' -H 'Content-Type:application/json' --insecure -v"
     result = os.system(command)
+    print("result of curl command: ",result)
     if result:
         break
     else:
