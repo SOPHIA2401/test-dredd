fd7524e4bfe2f528f0b6013b8653a39391cf6490
Sophia Garg <sofigarg@amazon.com>
2022-05-12 22:45:25 +0530

    67
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -4,10 +4,10 @@ for i in range(0,6):
     command = "curl 'https://admin:admin@localhost:9200' -H 'Content-Type:application/json' --insecure -v"
     result = os.system(command)
     print("result of curl command: ",result)
-    if result:
-        break
-    else:
+    if result !=0:
         os.system("sleep 30s")
+    else:
+        break
     print("i", i)    
 
 print("over")        
